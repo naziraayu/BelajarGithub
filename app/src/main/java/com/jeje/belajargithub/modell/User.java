e63e41937ea021afdcdb562d65d562b8656bb0cd
naziraayu <naziraayu2003@gmail.com>
2023-08-31 14:02:13 +0700

    CHANGE: - Menambah atribut pada class User

--- app/src/main/java/com/jeje/belajargithub/modell/User.java
+++ app/src/main/java/com/jeje/belajargithub/modell/User.java
@@ -1,4 +1,22 @@
 package com.jeje.belajargithub.modell;
 
 public class User {
+    private String name;
+    private String newId;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNewId() {
+        return newId;
+    }
+
+    public void setNewId(String newId) {
+        this.newId = newId;
+    }
 }
